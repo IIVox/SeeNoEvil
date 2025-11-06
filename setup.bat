5750acde1ec6431c41361d342716e7d73a897fd7
Vox <104469999+IIVox@users.noreply.github.com>
2025-11-06 18:59:52 +0000

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -4,5 +4,10 @@ echo Installing required libraries...
 python -m ensurepip
 python -m pip install --upgrade pip
 python -m pip install requests
+python -m pip install beautifulsoup4
+python3 -m ensurepip
+python3 -m pip install --upgrade pip
+python3 -m pip install requests
+python3 -m pip install beautifulsoup4
 echo Done.
 pause
